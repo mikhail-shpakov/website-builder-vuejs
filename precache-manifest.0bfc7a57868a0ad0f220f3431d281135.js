d3b72a5eee70cccba5332d6f532db952090f6da6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-12 22:26:46 +0000

    Deploy mikhail-shpakov/website-builder-vuejs to github.com/mikhail-shpakov/website-builder-vuejs.git:gh-pages

---
+++ precache-manifest.0bfc7a57868a0ad0f220f3431d281135.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93fdae99b92c0d52f69",
+    "url": "/website-builder-vuejs/css/app.592d22b5.css"
+  },
+  {
+    "revision": "e05fd942f5c300d43dd0",
+    "url": "/website-builder-vuejs/css/chunk-0e0ac1aa.3173eb82.css"
+  },
+  {
+    "revision": "393c7f9a86dddbbf5a24",
+    "url": "/website-builder-vuejs/css/chunk-285d9338.8d0dadc1.css"
+  },
+  {
+    "revision": "bdcd0680cc914a900e1e",
+    "url": "/website-builder-vuejs/css/chunk-6b0e2d61.dd156ca3.css"
+  },
+  {
+    "revision": "526cc56d028bba4e0a88",
+    "url": "/website-builder-vuejs/css/chunk-9d881552.c9664ba6.css"
+  },
+  {
+    "revision": "964028958e162b420efb",
+    "url": "/website-builder-vuejs/css/chunk-vendors.8617c10f.css"
+  },
+  {
+    "revision": "31d47085569e772c0f57aa8ec8381a5c",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.31d47085.woff"
+  },
+  {
+    "revision": "4a837d054b5f2a37170df8a275a13816",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.4a837d05.eot"
+  },
+  {
+    "revision": "b0fd91bb29dcb296a9a37f8bda0a2d85",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.b0fd91bb.ttf"
+  },
+  {
+    "revision": "f1997a8aba8a498fe4032e3b56e871ca",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.f1997a8a.woff2"
+  },
+  {
+    "revision": "3043d2ac6be3e302e3908617e1771f39",
+    "url": "/website-builder-vuejs/img/404.3043d2ac.svg"
+  },
+  {
+    "revision": "175d6dd5bfc76022297138708715aded",
+    "url": "/website-builder-vuejs/img/about.175d6dd5.svg"
+  },
+  {
+    "revision": "5365aa1e7c29c43496512a52302eeebb",
+    "url": "/website-builder-vuejs/img/desktop.5365aa1e.svg"
+  },
+  {
+    "revision": "3da65a95b392966b7afffb7fc70ed618",
+    "url": "/website-builder-vuejs/img/empty.3da65a95.svg"
+  },
+  {
+    "revision": "c58bae0b01d34399d0bb37a5d1429ee4",
+    "url": "/website-builder-vuejs/img/header.c58bae0b.svg"
+  },
+  {
+    "revision": "ec3dd86ae60ed660ae4206e17f559834",
+    "url": "/website-builder-vuejs/img/img.ec3dd86a.svg"
+  },
+  {
+    "revision": "bd61ee3f2ebfc32f492f4f21f152f741",
+    "url": "/website-builder-vuejs/img/logo.bd61ee3f.svg"
+  },
+  {
+    "revision": "2757bc6b2eb6a89307de51040f8a877f",
+    "url": "/website-builder-vuejs/img/start.2757bc6b.svg"
+  },
+  {
+    "revision": "95f568c0b63963d80e1d6f0d566a7497",
+    "url": "/website-builder-vuejs/img/text.95f568c0.svg"
+  },
+  {
+    "revision": "68efff953cfd8eaa0e28c0c67996b124",
+    "url": "/website-builder-vuejs/index.html"
+  },
+  {
+    "revision": "e93fdae99b92c0d52f69",
+    "url": "/website-builder-vuejs/js/app-legacy.19c65ddf.js"
+  },
+  {
+    "revision": "e05fd942f5c300d43dd0",
+    "url": "/website-builder-vuejs/js/chunk-0e0ac1aa-legacy.e24ff735.js"
+  },
+  {
+    "revision": "393c7f9a86dddbbf5a24",
+    "url": "/website-builder-vuejs/js/chunk-285d9338-legacy.3ced0679.js"
+  },
+  {
+    "revision": "bdcd0680cc914a900e1e",
+    "url": "/website-builder-vuejs/js/chunk-6b0e2d61-legacy.9b9b3313.js"
+  },
+  {
+    "revision": "526cc56d028bba4e0a88",
+    "url": "/website-builder-vuejs/js/chunk-9d881552-legacy.7afcd4c9.js"
+  },
+  {
+    "revision": "964028958e162b420efb",
+    "url": "/website-builder-vuejs/js/chunk-vendors-legacy.cefddcac.js"
+  },
+  {
+    "revision": "a23c81b009b5a862529980b0ed415d8f",
+    "url": "/website-builder-vuejs/manifest.json"
+  }
+]);
---
+++ precache-manifest.79613368007df3e6b76b1739f2af74fc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a384a9479841d4c946eea36f9ac8c39a",
+    "url": "/website-builder-vuejs/404.html"
+  },
+  {
+    "revision": "53f2e685cc9f47500357",
+    "url": "/website-builder-vuejs/css/app.592d22b5.css"
+  },
+  {
+    "revision": "e05fd942f5c300d43dd0",
+    "url": "/website-builder-vuejs/css/chunk-0e0ac1aa.3173eb82.css"
+  },
+  {
+    "revision": "45258d3c7106f19aa307",
+    "url": "/website-builder-vuejs/css/chunk-5922981a.c9664ba6.css"
+  },
+  {
+    "revision": "bdcd0680cc914a900e1e",
+    "url": "/website-builder-vuejs/css/chunk-6b0e2d61.dd156ca3.css"
+  },
+  {
+    "revision": "979a9be196550521d342",
+    "url": "/website-builder-vuejs/css/chunk-e11de690.8d0dadc1.css"
+  },
+  {
+    "revision": "52c95032e5b6f48186bc",
+    "url": "/website-builder-vuejs/css/chunk-vendors.8617c10f.css"
+  },
+  {
+    "revision": "31d47085569e772c0f57aa8ec8381a5c",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.31d47085.woff"
+  },
+  {
+    "revision": "4a837d054b5f2a37170df8a275a13816",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.4a837d05.eot"
+  },
+  {
+    "revision": "b0fd91bb29dcb296a9a37f8bda0a2d85",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.b0fd91bb.ttf"
+  },
+  {
+    "revision": "f1997a8aba8a498fe4032e3b56e871ca",
+    "url": "/website-builder-vuejs/fonts/materialdesignicons-webfont.f1997a8a.woff2"
+  },
+  {
+    "revision": "3043d2ac6be3e302e3908617e1771f39",
+    "url": "/website-builder-vuejs/img/404.3043d2ac.svg"
+  },
+  {
+    "revision": "175d6dd5bfc76022297138708715aded",
+    "url": "/website-builder-vuejs/img/about.175d6dd5.svg"
+  },
+  {
+    "revision": "5365aa1e7c29c43496512a52302eeebb",
+    "url": "/website-builder-vuejs/img/desktop.5365aa1e.svg"
+  },
+  {
+    "revision": "3da65a95b392966b7afffb7fc70ed618",
+    "url": "/website-builder-vuejs/img/empty.3da65a95.svg"
+  },
+  {
+    "revision": "c58bae0b01d34399d0bb37a5d1429ee4",
+    "url": "/website-builder-vuejs/img/header.c58bae0b.svg"
+  },
+  {
+    "revision": "ec3dd86ae60ed660ae4206e17f559834",
+    "url": "/website-builder-vuejs/img/img.ec3dd86a.svg"
+  },
+  {
+    "revision": "bd61ee3f2ebfc32f492f4f21f152f741",
+    "url": "/website-builder-vuejs/img/logo.bd61ee3f.svg"
+  },
+  {
+    "revision": "2757bc6b2eb6a89307de51040f8a877f",
+    "url": "/website-builder-vuejs/img/start.2757bc6b.svg"
+  },
+  {
+    "revision": "95f568c0b63963d80e1d6f0d566a7497",
+    "url": "/website-builder-vuejs/img/text.95f568c0.svg"
+  },
+  {
+    "revision": "6849df1dc95ef08f625f3667ad926a71",
+    "url": "/website-builder-vuejs/index.html"
+  },
+  {
+    "revision": "53f2e685cc9f47500357",
+    "url": "/website-builder-vuejs/js/app.0c6af38d.js"
+  },
+  {
+    "revision": "e05fd942f5c300d43dd0",
+    "url": "/website-builder-vuejs/js/chunk-0e0ac1aa.e24ff735.js"
+  },
+  {
+    "revision": "45258d3c7106f19aa307",
+    "url": "/website-builder-vuejs/js/chunk-5922981a.0e4d925b.js"
+  },
+  {
+    "revision": "bdcd0680cc914a900e1e",
+    "url": "/website-builder-vuejs/js/chunk-6b0e2d61.9b9b3313.js"
+  },
+  {
+    "revision": "979a9be196550521d342",
+    "url": "/website-builder-vuejs/js/chunk-e11de690.a4d66529.js"
+  },
+  {
+    "revision": "52c95032e5b6f48186bc",
+    "url": "/website-builder-vuejs/js/chunk-vendors.9071f521.js"
+  },
+  {
+    "revision": "a23c81b009b5a862529980b0ed415d8f",
+    "url": "/website-builder-vuejs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/website-builder-vuejs/robots.txt"
+  }
+]);
